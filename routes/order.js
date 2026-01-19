bf6f2360c008ebfc13da02ed0daf19bb20a9cf26
Shivam Mishra <shivammishra6339@gmail.com>
2026-01-19 21:21:53 +0530

    Added to get Orders via buyerId

--- routes/order.js
+++ routes/order.js
@@ -48,4 +48,21 @@ OrderRouter.post('/api/orders', async(req, res) => {
     }
 });
 
+OrderRouter.get('/api/orders/:buyerId', async (req, res) => {
+    try {
+        const{buyerId} = req.params;
+
+        const orders = await Order.find({buyerId});
+
+        if(orders.length == 0){
+            res.status(404).json({msg: "No orders found"});
+        }
+
+        return res.status(200).json(orders);
+
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
+});
+
 module.exports = OrderRouter;
